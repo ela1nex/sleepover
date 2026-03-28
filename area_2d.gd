7113f53014a32be1e50fb7601ab1450bb6331422
ela1nex <elainejxia@gmail.com>
2026-03-28 06:05:11 -0700

    whisper collection and font

--- area_2d.gd
+++ area_2d.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
 var float_time = 0.0
-
 func _process(delta: float) -> void:
+	body_entered.connect(_on_body_entered)
 	float_time += delta
 	position.y += sin(float_time*PI/2) * 0.1
+	
+func _on_body_entered(body) -> void:
+	if body.has_method("collect_whisper"):
+		body.collect_whisper(self)
+	
