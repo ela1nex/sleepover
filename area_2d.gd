566b0020cc844794681b4794061fcfc3b0c6b32d
ela1nex <elainejxia@gmail.com>
2026-03-28 10:45:01 -0700

    deposit system

--- area_2d.gd
+++ area_2d.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
 var float_time = 0.0
-func _process(delta: float) -> void:
+
+func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	
+func _process(delta: float) -> void:
 	float_time += delta
 	position.y += sin(float_time*PI/2) * 0.1
 	
