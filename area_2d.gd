d109ec0f3f93ba1cd8922d78aa54fd23e5904d48
ela1nex <elainejxia@gmail.com>
2026-03-28 04:51:02 -0700

    walking and animation

--- area_2d.gd
+++ area_2d.gd
@@ -2,7 +2,6 @@ extends Area2D
 
 var float_time = 0.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta: float) -> void:
 	float_time += delta
-	position.y += sin(float_time * 2.0) * 0.1
+	position.y += sin(float_time*PI/2) * 0.1
