635914a9765845e516b1530fcdfab08c01c62e3e
ela1nex <elainejxia@gmail.com>
2026-03-28 02:52:56 -0700

    tilemap, glow, movement

--- area_2d.gd
+++ area_2d.gd
@@ -5,4 +5,4 @@ var float_time = 0.0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	float_time += delta
-	position.y += sin(float_time * 2.0) * 0.2
+	position.y += sin(float_time * 2.0) * 0.1
