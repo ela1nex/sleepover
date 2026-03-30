9087ab78cab1211f3331993f33dc1b7390468f07
ela1nex <elainejxia@gmail.com>
2026-03-29 23:46:05 -0700

    fix: glitch again

--- area_2d.gd
+++ area_2d.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
+signal first_collected
+
 
 var float_time = 0.0
+var first = false
 
 func _ready() -> void:
 	position.y -= 5
@@ -12,6 +15,7 @@ func _process(delta: float) -> void:
 	position.y += sin(float_time*PI/2) * 0.1
 	
 func _on_body_entered(body) -> void:
-	if body.has_method("collect_whisper"):
-		if body.collect_whisper(self):
-			self.queue_free()
+	if body.has_method("collect_whisper") and body.collect_whisper(self):
+		if first == false:
+			emit_signal("first_collected") 
+		queue_free()
